255f7f535cb49399fb58e85d140d337a95f0f422
kajo404 <jonas.kaltenbach@googlemail.com>
2016-03-12 11:14:09 +0100

    corrected use of callback functions & code cleanup

--- db/db-access.js
+++ db/db-access.js
@@ -2,34 +2,25 @@ var request = require('request');
 //URL to the API provided by Project A
 var url = 'https://got-api.bruck.me/api/episodes/find';
 
-
-//dateString is in format: "2011-04-16T22:00:00.000Z"
-var makeDateObject = function(dateString){
-	var year = dateString.slice(0,4);
-	var month = parseInt(dateString.slice(5,7))-1;
-	var day = parseInt(dateString.slice(8,10))+1;
-	var hours = dateString.slice(11,13);
-	var minutes = dateString.slice(14,16);
-	var date = new Date(year,month,day,hours,minutes);
-	return date;
-
-};
-var airDate = function(season, episode, callback){
+/*
+The callback function must take a date object as a parameter
+*/
+module.exports.airDate = function(season, episode, callback){
+	//Form includes the search criteria 
 	var form =  {
-	form: {
-		'season': season,
-		'nr' : episode
+		form: {
+			'season': season,
+			'nr' : episode
 		}	
 	};
+	//Make POST request to API
 	var answer = request.post(url,form, function(err, resp, body){
+		//just get the airing date information
 		var json = JSON.parse(body);
-		var airDate = json.data[0].airDate;
+		var airDate = json.data[0].airDate; //dateString is in format: "2011-04-16T22:00:00.000Z"
 		if(!err && resp.statusCode === 200){
-			callback(airDate);
+			//make a Date object for the callback function to use
+			callback(new Date(airDate));
 		}
 	});
-};
-module.exports.getAirDate = function(season, episode){
-	airDate(season, episode, makeDateObject);
-	console.log(airDate);
 };
