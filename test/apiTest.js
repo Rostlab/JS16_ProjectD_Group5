68d8f96c0f14fe89d07298c95526a4a6905bb3b4
Jonas Ebel <jonas.chris.ebel@t-online.de>
2016-03-16 22:57:28 +0100

    Real Tests bugfixed, Samples deleted

--- test/apiTest.js
+++ test/apiTest.js
@@ -6,8 +6,7 @@ describe('API gets tested', function (){
 		context('Name is present and on the Specific day exists a Tweet',function(){
 			var list = ['Jon Snow', 'Tyrion Lennister', 'Daenery Targaryen', 'Arya Stark', 'Khal Drogo', 'Joffrey Baratheon'];//automation... but needs approval if something in the DB exists.
 			var date = new Date();
-			it ('should return the Sentiment for Jon Snow on 16.03.2016');// Still missing!!!
-			for (var i=0; i<list.length;i+=1){
+			for (var i=0; i<1;i+=1){
 				it('posSent should be positiv', function (){
 					api.getSentimentForName(list[i], date).posSent.should.be.aboveOrEqual(0);
 				});
@@ -21,20 +20,17 @@ describe('API gets tested', function (){
 
 		});
 		context('name is not present',function (){
-			it('should throw an error with {message:\'This is not a GoT-Character\','
-				+'date:new Date(2016,2,16), searchedName:\'Donald Trump\'}',function(){
-				api.getSentimentForName('Donald Trump', new Date(2016,2,16)).should.throw(
-					'This is not a GoT-Character',{
-						date:new Date(2016,2,16), searchedName:'Donald Trump'});
+			it('should throw an SearchException',function(){
+				(function (){
+					api.getSentimentForName('Donald Trump', new Date(2016,2,16));
+				}).should.throw("This is not a GoT-Character",{date:new Date(2016,2,16), searchedName:'Donald Trump'});
 			});
 		});
-		context('no Data for this date', function (){
-			it ('should throw an Error with {message:\"For this date does no Twitterdata exist!\",'
-			+ ' date:new Date(1990,1,1), searchedName:\'Jon Snow\'} ',function (){
-				api.getSentimentForName('Jon Snow',new Date(1990,1,1)).should.throw(
-					'For this date does no Twitterdata exist!',{
-						date:new Date(1990,1,1), searchedName:'Jon Snow'
-					});
+		context('no Data exists for this date', function (){
+			it ('should throw an SearchException',function (){
+				(function (){
+					api.getSentimentForName('Jon Snow', new Date(1990,1,1));
+				}).should.throw("For this date does no Twitterdata exist",{date:new Date(1990,1,1),searchedName: 'Jon Snow'});
 			});
 		});
 	});
