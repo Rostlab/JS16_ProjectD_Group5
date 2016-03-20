9605f3f4fc78a3751c17dd0aac0cd65bd1e4edce
kajo404 <jonas.kaltenbach@googlemail.com>
2016-03-20 10:04:02 +0100

    start / stop automation

--- logic/automate.js
+++ logic/automate.js
@@ -2,19 +2,30 @@ var names = require('../popChars/popChars.json');
 var twitter = require('./twitter_rest');
 var minutes = 0.001; // time interval for twitter api calls
 
-var interval = minutes * 60 * 1000; //to milliseconds
+var interval;
 
 var currentPos = 0;
 /*
-Runs REST analysis for characters in popChars.json for the last day.
-Function is executed approx once per day per character
+ Runs REST analysis for characters in popChars.json for the last day.
+ Function is executed approx once per day per character
  */
-setInterval(function(){
 
-    var currentDate = new Date();
-    var yesterday = currentDate.setDate(currentDate.getDay()-1);
+exports.startAutomation = function () {
 
-    twitter.launchSearch(names[currentPos].name, yesterday, currentDate);
-    currentPos = (currentPos +1)%names.length;
+    setInterval(function () {
 
-}, interval); // interval is set here
+        var currentDate = new Date();
+        var yesterday = currentDate.setDate(currentDate.getDay() - 1);
+
+        twitter.launchSearch(names[currentPos].name, yesterday, currentDate);
+        currentPos = (currentPos + 1) % names.length;
+    }, interval); // interval is set here
+}; //TODO optional timeframe parameter with default 12 minutes
+
+exports.stopAutomation = function () {
+
+};
+
+
+
+
