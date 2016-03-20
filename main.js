669abe9b2fdffade1064efd770c76df178ebc8d0
kajo404 <jonas.kaltenbach@googlemail.com>
2016-03-20 13:52:01 +0100

    introduced configured variable again :P

--- main.js
+++ main.js
@@ -1,30 +1,20 @@
 var fs = require('fs');
+var configured = false;
+var retObject;
 module.exports = {
     init: function (params) {
-        var config = {
-            "twitter": {
-                "consumer_key": params.consumerkey,
-                "consumer_secret": params.consumersecret,
-                "access_token_key": params.accesstokeykey,
-                "access_token_secret": params.accesstokensecret
-            },
-            "database": {
-                "user": params.user,
-                "password": params.password,
-                "port": params.port,
-                "uri": params.uri,
-                "name": params.name
-            },
-            "databaseA": {
-                "token": params.tokenToA
-            }
-        };
-        fs.writeFileSync(filename, JSON.stringify(config, null, 4), function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                return require('./index.js');
-            }
-        });
+        if (!configured) {
+            fs.writeFileSync(params, JSON.stringify(config, null, 4), function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    configured = true;
+                }
+            });
+            retObject = require('./index.js');
+            return retObject;
+        } else {
+            return retObject;
+        }
     }
 };
