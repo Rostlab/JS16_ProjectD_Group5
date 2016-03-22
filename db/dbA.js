e3a669fa7c7f85a26fde76f1124c8dbae5315d96
kajo404 <jonas.kaltenbach@googlemail.com>
2016-03-22 15:26:20 +0100

    removed twitter database

--- db/dbA.js
+++ db/dbA.js
@@ -33,7 +33,7 @@ exports.saveSentiment = function (charName, json) {
 
 exports.airDate = function (season, episode, callback) {
     //URL to the API provided by Project A
-    var url = 'https://got-api.bruck.me/api/episodes/find/' + tokenString;
+    var url = config.databaseA.airDateURL + tokenString;
     //Form includes the search criteria
     var form = {
         form: {
@@ -59,7 +59,7 @@ exports.airDate = function (season, episode, callback) {
  */
 exports.characterNames = function (callback) {
     //URL to API by ProjectA
-    var url = 'https://got-api.bruck.me/api/characters/' + tokenString;
+    var url = config.databaseA.characterNamesURL + tokenString;
     //GET request to API
     request.get(url, function (err, resp, body) {
         //check foŕ valid response
