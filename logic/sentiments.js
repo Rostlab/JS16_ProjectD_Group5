9e083f77e9df1873aaae3f6fb3431a110d0fb8dc
Camille Mainz <camille@mainz.me>
2016-03-22 03:33:42 +0100

    intial draft for sentiments.js

--- logic/sentiments.js
+++ logic/sentiments.js
@@ -2,3 +2,54 @@ var dbA = require('../db/dbA');
 var dbTweets = require('../db/dbTweets');
 var sentiment = require('sentiment');
 
+var date;
+var name;
+var posSentiment;
+var negSentiment;
+var posTweets;
+var negTweets;
+var nullTweets;
+
+exports.getSentimentsForTweets = function (characterName, startDate, endDate) {
+
+    date = startDate;
+    name = characterName;
+
+    var jsonParams = {};
+    jsonParams.characterName = characterName;
+    jsonParams.startDate = startDate;
+    jsonParams.endDate = endDate;
+
+    return dbTweets.getTweets(jsonParams, analyzeTweets);
+}
+
+function analyzeTweets (tweets) {
+    for (var index in tweets) {
+        var currentTweet = tweets[index];
+        var sentimentScore = sentiment(currentTweet.text);
+
+        if (sentimentScore > 0) {
+            posSentiment += sentimentScore;
+            posTweets += 1;
+        } else if (sentimentScore < 0) {
+            negSentiment += sentimentScore;
+            negTweets += 1;
+        } else {
+            nullTweets += 1;
+        }
+    }
+
+}
+
+function saveSentiments() {
+
+    var sentimentJSON = {};
+    sentimentJSON.date = date;
+    sentimentJSON.posSentiment = posSentiment;
+    sentimentJSON.negSentiment = negSentiment;
+    sentimentJSON.posTweets = posTweets;
+    sentimentJSON.negTweets = negTweets;
+    sentimentJSON.nullTweets = nullTweets;
+
+    dbA.saveSentiment(name, sentimentJSON);
+}
