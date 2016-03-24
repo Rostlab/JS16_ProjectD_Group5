e63b5e782d21fe175691dd678dca2189b1d9fbf7
kajo404 <jonas.kaltenbach@googlemail.com>
2016-03-24 18:12:13 +0100

    moved configured = true to the end to ensure completion

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ module.exports = {
     init: function (params) {
         if (!configured) {
             fs.writeFileSync(require('path').resolve(__dirname, './cfg/config.json'), JSON.stringify(params, null, 4));
-            configured = true;
             retObject = require('./index.js');
+            configured = true;
         }
         return retObject;
     }
