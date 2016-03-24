812a7a101f1b0138314f126d1658e0ce08d27fb5
kajo404 <jonas.kaltenbach@googlemail.com>
2016-03-24 18:07:48 +0100

    fixed config file path

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var retObject;
 module.exports = {
     init: function (params) {
         if (!configured) {
-            fs.writeFileSync(require('path').resolve('./cfg/config.json'), JSON.stringify(params, null, 4));
+            fs.writeFileSync(require('path').resolve(__dirname, './cfg/config.json'), JSON.stringify(params, null, 4));
             configured = true;
             retObject = require('./index.js');
         }
