9d1cd25ebe7542d755d0e8e82efd4f325da10baa
kajo404 <jonas.kaltenbach@googlemail.com>
2016-03-28 12:38:00 +0200

    formatting to improve readability

--- logic/twitterAPI.js
+++ logic/twitterAPI.js
@@ -5,10 +5,10 @@ var sentiments = require('./sentiments');
 //local config must be in /cfg/config.json. See configDUMMY.json for example
 var config = require('../cfg/config.json');
 var apiAccess = {
-  	consumer_key: config.twitter.consumer_key,
-  	consumer_secret: config.twitter.consumer_secret,
-  	access_token_key: config.twitter.access_token_key,
-  	access_token_secret: config.twitter.access_token_secret
+    consumer_key: config.twitter.consumer_key,
+    consumer_secret: config.twitter.consumer_secret,
+    access_token_key: config.twitter.access_token_key,
+    access_token_secret: config.twitter.access_token_secret
 };
 
 var client = new twitter(apiAccess);
@@ -16,44 +16,44 @@ var client = new twitter(apiAccess);
 
 //launch Streaming-API search
 //timeFrame in seconds
-exports.getStream = function(characterName, duration, isSaved, callback) {
+exports.getStream = function (characterName, duration, isSaved, callback) {
     var tweetArray = [];
     var startTime = new Date();
     var currentDate = getCurrentDateAsString();
     var trimmedCharacterName = removeParentheses(characterName);
-	client.stream('statuses/filter', {track: trimmedCharacterName}, function(stream) {
-  	stream.on('data', function(tweet) {
-      tweetArray.push(tweet);
-      var currentTime = new Date();
-      if(currentTime.getTime()>=(startTime.getTime()+duration*1000)){
-          console.log('Timelimit reached!');
-          runSentimentAnalysis(tweetArray, characterName, currentDate, currentDate, isSaved, callback);
-          stream.destroy();
-      }
+    client.stream('statuses/filter', {track: trimmedCharacterName}, function (stream) {
+        stream.on('data', function (tweet) {
+            tweetArray.push(tweet);
+            var currentTime = new Date();
+            if (currentTime.getTime() >= (startTime.getTime() + duration * 1000)) {
+                console.log('Timelimit reached!');
+                runSentimentAnalysis(tweetArray, characterName, currentDate, currentDate, isSaved, callback);
+                stream.destroy();
+            }
+        });
+        stream.on('error', function (error) {
+            throw error;
+        });
     });
-  	stream.on('error', function(error) {
-    	throw error;
-  	});
-  });
 };
 
 //launch Rest-API search
 //startDate, endDate in format "yyyy-mm-dd"
-exports.getRest = function(characterName, startDate, endDate, isSaved, callback) {
+exports.getRest = function (characterName, startDate, endDate, isSaved, callback) {
     var trimmedCharacterName = removeParentheses(characterName);
     var searchArguments = getRestSearchArguments(trimmedCharacterName, startDate, endDate);
-    client.get('search/tweets', searchArguments, function(error, tweets, response){
-      var statuses = tweets.statuses;
-      var tweetArray = [];
-      for (var index in statuses) {
-        var tweet = statuses[index];
-        tweetArray.push(tweet);
-      }
-      runSentimentAnalysis(tweetArray, characterName, startDate, endDate, isSaved, callback);
+    client.get('search/tweets', searchArguments, function (error, tweets, response) {
+        var statuses = tweets.statuses;
+        var tweetArray = [];
+        for (var index in statuses) {
+            var tweet = statuses[index];
+            tweetArray.push(tweet);
+        }
+        runSentimentAnalysis(tweetArray, characterName, startDate, endDate, isSaved, callback);
     });
 };
 
-function runSentimentAnalysis(tweetArray, characterName, startDate, endDate, isSaved, callback){
+function runSentimentAnalysis(tweetArray, characterName, startDate, endDate, isSaved, callback) {
     var jsonTweets = getJSONTweetArray(tweetArray, characterName);
     sentiments.calculateSentimentsForTweets(characterName, jsonTweets, startDate, endDate, isSaved, callback);
 }
@@ -66,20 +66,20 @@ function getJSONTweetArray(tweetArray, characterName) {
     return JSONArray;
 }
 
-function getTweetAsJSON(tweet, characterName){
-  var jsonTweet = {};
-  // jsonTweet.id = tweet.id_str;
-  // jsonTweet.characterName = characterName;
-  jsonTweet.created_at = tweet.created_at;
-  jsonTweet.text = tweet.text;
-  jsonTweet.retweeted = tweet.retweet_count;
-  jsonTweet.fav = tweet.favorite_count;
-  // jsonTweet.lang = tweet.lang;
-  return jsonTweet;
+function getTweetAsJSON(tweet, characterName) {
+    var jsonTweet = {};
+    // jsonTweet.id = tweet.id_str;
+    // jsonTweet.characterName = characterName;
+    jsonTweet.created_at = tweet.created_at;
+    jsonTweet.text = tweet.text;
+    jsonTweet.retweeted = tweet.retweet_count;
+    jsonTweet.fav = tweet.favorite_count;
+    // jsonTweet.lang = tweet.lang;
+    return jsonTweet;
 }
 
 function getRestSearchArguments(character, startDate, endDate) {
-  return {q: character, result_type: 'mixed', since: startDate, until: endDate, lang: 'en', count: 100};
+    return {q: character, result_type: 'mixed', since: startDate, until: endDate, lang: 'en', count: 100};
 }
 
 function getCurrentDateAsString() {
@@ -88,7 +88,7 @@ function getCurrentDateAsString() {
     if (currentMonth < 10) {
         currentMonth = "0" + currentMonth;
     }
-    var dateString = currentDate.getFullYear() + "-" +  currentMonth + "-" + currentDate.getDate();
+    var dateString = currentDate.getFullYear() + "-" + currentMonth + "-" + currentDate.getDate();
     return dateString;
 }
 
