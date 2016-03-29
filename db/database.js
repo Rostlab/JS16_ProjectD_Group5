fab90128d292c0718b1846370a16cebf3677ad1c
kajo404 <jonas.kaltenbach@googlemail.com>
2016-03-29 18:35:00 +0200

    fixed getSentimentForNameTimeframe

--- db/database.js
+++ db/database.js
@@ -53,10 +53,13 @@ exports.getSentimentForNameTimeframe = function (charName, startDate, endDate, c
     var url = config.database.sentimentGetChar;
     var startmil = (new Date(startDate)).getTime();
     var endmil = (new Date(endDate)).getTime();
-    request.get(url, function (err, resp, body) {
-        //check for valid response
+    var form = {
+        form: {
+            'character': charName
+        }
+    };
+    request.post(url, form, function (err, resp, body) {
         if (!err && resp.statusCode === 200) {
-            //parse answer String to a JSON Object
             var json = JSON.parse(body);
             json.filter(function (element) {
                 var date = new Date(element.date).getTime();
@@ -64,7 +67,6 @@ exports.getSentimentForNameTimeframe = function (charName, startDate, endDate, c
                 var groupname = element.description === "Group 5";
                 return dateframe && groupname; //only includes results from our group
             });
-            //give JSON object to the callback function
             callback(json);
         }
     });
