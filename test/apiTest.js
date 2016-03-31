f73a9388103924f4c17765dcacd47e6f1e169226
Jonas Ebel <jonas.chris.ebel@t-online.de>
2016-03-31 11:55:55 +0200

    increased the timeout

--- test/apiTest.js
+++ test/apiTest.js
@@ -14,6 +14,7 @@ setTimeout(function(){//This function is needed to get all the requires straight
 		}
 	}
 	describe('API gets tested', function (){
+		this.timeout(300);
 		var date = new Date();
 		describe('#init',function(){
 			it('the api-object should be filled already',function(){
